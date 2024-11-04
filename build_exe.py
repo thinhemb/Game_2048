90fa18b6d0f1c5ca577a689678f0a27755db5aa6
thinhemb <thinhdo19022001@gmail.com>
2024-11-04 10:43:10 +0700

    update build_exe.py

--- build_exe.py
+++ build_exe.py
@@ -2,13 +2,13 @@ import PyInstaller.__main__
 import PyInstaller.config
 import os
 
-distpath ="--distpath=" + str(os.getcwd()) +  "\\builder"
-workpath = "--workpath=" + str(os.getcwd()) +  "\\builder" + "\\tempo"
+distpath ="--distpath=" + os.path.join(str(os.getcwd()) ,"builder")
+workpath = "--workpath=" + os.path.join(str(os.getcwd()),  "builder/tempo")
 # PyInstaller.config.CONF['distpath'] = path
 PyInstaller.__main__.run([
     'main.py',
-    '--onedir',
-    # '--onefile',
+    # '--onedir',
+    '--onefile',
     '-nGame_2048',
     # '--hidden-import=pya2l.cgen',
     '--windowed',
